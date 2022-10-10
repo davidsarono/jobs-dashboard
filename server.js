4e7c898ecf37e586abd46972d7b09ef3f5b6c396
davidsarono <davidsarono@refactory.id>
2022-10-11 02:49:53 +0700

    feat: add logger

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const winston = require('winston');
+const expressWinston = require('express-winston');
 const path = require('path');
+
 const assetsRouter = require('./server/assets-router');
 const routes = require('./server/routes/index.route');
 
@@ -10,13 +13,28 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `${timestamp} ${level}: ${message}`;
+    })
+  ),
+  msg: "HTTP {{req.method}} {{req.url}}",
+  expressFormat: true,
+  colorize: true,
+}));
+
 app.use('/', routes);
 
 app.use('/assets', assetsRouter);
 
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+});
 
 const { PORT = 5000 } = process.env;
 
